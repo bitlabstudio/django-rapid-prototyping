2cb38f592b390293771440912fc59ca09ca2e67e
Martin Brochhaus <mbrochh@gmail.com>
2013-09-03 09:28:08 +0200

    Fixed Difference totals: now based on the time totals, not the difference average

--- rapid_prototyping/static/rapid_prototyping/js/calculate_total.js
+++ rapid_prototyping/static/rapid_prototyping/js/calculate_total.js
@@ -1,15 +1,12 @@
-function calculate_average_for_column(container, item_cell, total_cell) {
-    // Calculates the average over all `item_cell` values and adds it to the
-    // `total_cell`.
-    var total = 0;
-    var count = 0;
-    $(container).find(item_cell).each(function() {
-        total += parseFloat($(this).html());
-        count ++;
-    });
-    var average = total / count;
-    console.log(average.toFixed(2));
-    $(container).find(total_cell).html(average.toFixed(2));
+function calculate_difference_totals(container) {
+    var actual_total = $(container).find('.costsActualTimeTotal').html();
+    var developer_total = $(container).find('.costsDeveloperTimeTotal').html();
+    var estimated_total = $(container).find('.costsTimeTotal').html();
+
+    var developer_difference_total = actual_total / developer_total;
+    var estimated_difference_total = actual_total / estimated_total;
+    $(container).find('.costsDeveloperDifferenceTotal').html(developer_difference_total.toFixed(2));
+    $(container).find('.costsEstimatedDifferenceTotal').html(estimated_difference_total.toFixed(2));
 }
 
 function calculate_total_for_column(container, item_cell, total_cell) {
@@ -31,5 +28,5 @@ function calculate_total(container) {
     calculate_total_for_column(container, '.costsDeveloperCosts', '.costsDeveloperCostsTotal');
     calculate_total_for_column(container, '.costsActualTime', '.costsActualTimeTotal');
     calculate_total_for_column(container, '.costsActualCosts', '.costsActualCostsTotal');
-    calculate_average_for_column(container, '.costsDifference', '.costsDifferenceTotal');
+    calculate_difference_totals(container);
 }
